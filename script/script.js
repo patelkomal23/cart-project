e859da1c6f7b0e5fef022b379dc1c87b2681a20e
patelkomal23 <patelkomal2332@gmail.com>
2025-04-07 17:54:02 +0530

    d

---
+++ script/script.js
@@ -0,0 +1,49 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Simple add to cart
+document.querySelectorAll(".add-card").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const card = btn.closest(".card");
+        const name = card.querySelector(".card-title").innerText;
+        const price = card.querySelector("span").innerText;
+        const img = card.querySelector("img").src;
+
+        const item = cart.find(i => i.name === name);
+
+        if (item) {
+            item.qty += 1;
+        } else {
+            cart.push({ name, price, img, qty: 1 });
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        showCart(); // Update the cart display
+    });
+});
+
+// Show cart in table
+function showCart() {
+    const cartTable = document.getElementById("cartTable");
+    cartTable.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        cartTable.innerHTML += `
+            <tr>
+                <td><img src="${item.img}" width="40"><br>${item.name}</td>
+                <td>${item.qty}</td>
+                <td>${item.price}</td>
+                <td><button onclick="removeItem(${index})" class="btn btn-sm btn-danger">x</button></td>
+            </tr>
+        `;
+    });
+}
+
+// Remove item
+function removeItem(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    showCart();
+}
+
+// Show cart on page load
+showCart();
